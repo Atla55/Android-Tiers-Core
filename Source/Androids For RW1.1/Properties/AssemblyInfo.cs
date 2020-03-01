6b92afaa7460d679b7f5f8defa2ae44860e11b3b
aRandomKiwi <15568941+Kiwy404@users.noreply.github.com>
2020-03-01 06:43:00 +0100

    Fix Harmony 2.x critical issues
    -See the author's post for more information : https://www.reddit.com/r/RimWorld/comments/fbnm45/harmony_the_full_story/

--- Source/Androids For RW1.1/Properties/AssemblyInfo.cs
+++ Source/Androids For RW1.1/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyVersion("2.0.2.0")]
 [assembly: CompilationRelaxations(8)]
 [assembly: RuntimeCompatibility(WrapNonExceptionThrows = true)]
 [assembly: Debuggable(DebuggableAttribute.DebuggingModes.Default | DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints | DebuggableAttribute.DebuggingModes.EnableEditAndContinue | DebuggableAttribute.DebuggingModes.DisableOptimizations)]
@@ -17,4 +17,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("4ed7b8ad-47e8-4436-a9bf-e664b17196d7")]
-[assembly: AssemblyFileVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.2.0")]
