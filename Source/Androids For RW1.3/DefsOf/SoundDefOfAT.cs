2d50039e97f8d6612010033c9d40820441498710
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2021-07-20 20:56:46 +0200

    Change tending sound for androids

---
+++ Source/Androids For RW1.3/DefsOf/SoundDefOfAT.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using RimWorld;
+using Verse;
+
+namespace MOARANDROIDS
+{
+    [DefOf]
+    public static class SoundDefOfAT
+    {
+        public static SoundDef Recipe_Smith;
+    }
+}
