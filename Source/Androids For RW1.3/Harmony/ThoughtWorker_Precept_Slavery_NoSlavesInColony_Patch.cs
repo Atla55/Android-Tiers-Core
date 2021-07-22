417aa04168dc3c64251573ca3c982b381211e95d
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2021-07-22 07:01:13 +0200

    Add Ideology and Royalty DLC support for most of Thoughts handling by androids

---
+++ Source/Androids For RW1.3/Harmony/ThoughtWorker_Precept_Slavery_NoSlavesInColony_Patch.cs
@@ -0,0 +1,30 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MOARANDROIDS
+{
+    internal class ThoughtWorker_Precept_Slavery_NoSlavesInColony_Patch
+    {
+        /*
+         * Basic androids dont care about nudity stuff
+         */
+        [HarmonyPatch(typeof(ThoughtWorker_Precept_Slavery_NoSlavesInColony), "ShouldHaveThought")]
+        public class CurrentStateInternal_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(Pawn p, ref ThoughtState __result)
+            {
+                if (Utils.ExceptionAndroidListBasic.Contains(p.def.defName))
+                {
+                    __result = ThoughtState.Inactive;
+                }
+            }
+        }
+    }
+}
---
+++ Source/Androids For RW1.3/Harmony/ThoughtWorker_StyleDominance_Patch.cs
@@ -0,0 +1,31 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MOARANDROIDS
+{
+    internal class ThoughtWorker_StyleDominance_Patch
+
+    {
+        /*
+         * Basic androids dont care about nudity stuff
+         */
+        [HarmonyPatch(typeof(ThoughtWorker_StyleDominance), "ShouldHaveThought")]
+        public class CurrentStateInternal_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(Pawn p, ref ThoughtState __result)
+            {
+                if (Utils.ExceptionAndroidListBasic.Contains(p.def.defName))
+                {
+                    __result = ThoughtState.Inactive;
+                }
+            }
+        }
+    }
+}
