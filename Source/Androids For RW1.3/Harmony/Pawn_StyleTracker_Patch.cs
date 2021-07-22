71b8503ff1b29b56a7bf00a2eee892f0b2d8e868
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2021-07-22 09:24:59 +0200

    Fix (Ideology) prevent androids from requesting to change their style

---
+++ Source/Androids For RW1.3/Harmony/Pawn_StyleTracker_Patch.cs
@@ -0,0 +1,26 @@
+﻿using Verse;
+using HarmonyLib;
+using RimWorld;
+using System;
+
+namespace MOARANDROIDS
+{
+    internal class Pawn_StyleTracker_Patch
+    {
+        /*
+         * Prevent androids && VX0 surrogates from wanting to change their look
+         */
+        [HarmonyPatch(typeof(Pawn_StyleTracker), "get_CanDesireLookChange")]
+        public class CanDesireLookChange_Patch
+        {
+            [HarmonyPostfix]
+            public static void Listener(ref bool __result, Pawn ___pawn)
+            {
+                if (___pawn.IsAndroidTier() || Utils.VX0ChipPresent(___pawn))
+                {
+                    __result = false;
+                }
+            }
+        }
+    }
+}
