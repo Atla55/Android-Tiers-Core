e033cc34addfa9cb2e19ba366fa08fdb6e1598f9
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2021-08-01 16:52:01 +0200

    Fix basic androids making other bigots colonists mood debuff
    -Maybe need to watch at HAR what they can do in the future in their API to prevent ideos, because the fix use some prefix, which can lead to other mods incompatibility and can easily be broken by other mods

---
+++ Source/Androids For RW1.3/Harmony/ThoughtWorker_Precept_IdeoDiversity_Patch.cs
@@ -0,0 +1,50 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+using UnityEngine;
+
+namespace MOARANDROIDS
+{
+    internal class ThoughtWorker_Precept_IdeoDiversity_Patch
+    {
+        [HarmonyPatch(typeof(ThoughtWorker_Precept_IdeoDiversity), "ShouldHaveThought")]
+        public class TW_Precept_IdeoDiversity_ShouldHaveThought
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn p, ref ThoughtState __result, ThoughtWorker_Precept_IdeoDiversity __instance)
+            {
+                if (p.Faction == null || !p.IsColonist)
+                {
+                    __result = false;
+                    return false;
+                }
+                int num = 0;
+                int num2 = 0;
+                List<Pawn> list = p.Map.mapPawns.SpawnedPawnsInFaction(p.Faction);
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (!list[i].IsQuestLodger() && list[i].RaceProps.Humanlike && !list[i].IsSlave && !list[i].IsPrisoner && !list[i].IsBasicAndroidTier())
+                    {
+                        num2++;
+                        if (list[i] != p && list[i].Ideo != p.Ideo)
+                        {
+                            num++;
+                        }
+                    }
+                }
+                if (num == 0)
+                {
+                    __result = ThoughtState.Inactive;
+                    return false;
+                }
+                __result = ThoughtState.ActiveAtStage(Mathf.RoundToInt((float)num / (float)(num2 - 1) * (float)(__instance.def.stages.Count - 1)));
+                return false;
+            }
+        }
+    }
+}
---
+++ Source/Androids For RW1.3/Harmony/ThoughtWorker_Precept_IdeoDiversity_Uniform_Patch.cs
@@ -0,0 +1,46 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MOARANDROIDS
+{
+    internal class ThoughtWorker_Precept_IdeoDiversity_Uniform_Patch
+    {
+        [HarmonyPatch(typeof(ThoughtWorker_Precept_IdeoDiversity_Uniform), "ShouldHaveThought")]
+        public class TW_Precept_IdeoUniform_ShouldHaveThought
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn p, ref ThoughtState __result)
+            {
+                if (p.Faction == null || !p.IsColonist)
+                {
+                    __result = false;
+                    return false;
+                }
+                List<Pawn> list = p.Map.mapPawns.SpawnedPawnsInFaction(p.Faction);
+                int num = 0;
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (list[i] != p && list[i].RaceProps.Humanlike && !list[i].IsSlave && !list[i].IsQuestLodger() && !list[i].IsBasicAndroidTier())
+                    {
+                        if (list[i].Ideo != p.Ideo)
+                        {
+                            Log.Message("=>" + list[i].LabelCap);
+                            __result = false;
+                            return false;
+                        }
+                        num++;
+                    }
+                }
+
+                __result = num > 0;
+                return false;
+            }
+        }
+    }
+}
