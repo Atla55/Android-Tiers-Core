748ea1962da87ab511028ccd3e22a428a0977ccd
aRandomKiwi <15568941+aRandomKiwi@users.noreply.github.com>
2021-08-07 15:41:46 +0200

    Fix some NULL exception on basic androids spawn (Ritual related stuff)

---
+++ Source/Androids For RW1.3/Harmony/RitualRoleConvertee_Patch.cs
@@ -0,0 +1,30 @@
+﻿using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+namespace MOARANDROIDS
+{
+    internal class RitualRoleConvertee_Patch
+    {
+        [HarmonyPatch(typeof(RitualRoleConvertee), "AppliesToPawn")]
+        public class PawnObserver_PossibleToObserve_Patch
+        {
+            [HarmonyPrefix]
+            public static bool Listener(Pawn p, out string reason, LordJob_Ritual ritual, RitualRoleAssignments assignments, Precept_Ritual precept, bool skipReason, ref bool __result)
+            {
+                reason = null;
+                if (p == null || p.IsBasicAndroidTier() || p.IsSurrogateAndroid(false, true))
+                {
+                    __result = false;
+                    return false;
+                }
+                return true;
+            }
+        }
+    }
+}
